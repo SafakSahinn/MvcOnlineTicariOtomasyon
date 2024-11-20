56592d64b9021f5fecc356794f4f6de2398ee930
safak <safaksahin022@gmail.com>
2024-11-20 19:13:48 +0300

    Gider

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Gider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Gider
+    {
+        [Key]
+        public int GiderId { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime Tarih { get; set; }
+        public decimal Tutar { get; set; }
+    }
+}
