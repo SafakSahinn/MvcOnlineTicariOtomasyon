9106a5dd52948f897a23a207b9dd17031189f8cb
safak <safaksahin022@gmail.com>
2024-11-20 19:17:53 +0300

    SatisHareket

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/SatisHareket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class SatisHareket
+    {
+        public int SaitsId { get; set; }
+        //ürün
+        //cari
+        //personel
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+    }
+}
