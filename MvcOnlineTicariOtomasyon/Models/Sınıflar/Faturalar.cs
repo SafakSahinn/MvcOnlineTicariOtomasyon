ad834389d0113a270191076a48f13440113afdb6
safak <safaksahin022@gmail.com>
2024-11-20 19:06:22 +0300

    Fatura

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Faturalar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Faturalar
+    {
+        public int FaturaId { get; set; }
+        public string FarutaSeriNo { get; set; }
+        public string FaturaSıraNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public string VergiDairesi { get; set; }
+        public DateTime Saat { get; set; }
+        public string TeslimEden { get; set; }
+        public string TeslimAlan { get; set; }
+    }
+}
