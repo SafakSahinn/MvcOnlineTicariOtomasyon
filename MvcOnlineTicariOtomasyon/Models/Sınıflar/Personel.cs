15bc61246c117fffa34867cb5e4383c68d785ef2
safak <safaksahin22@outlook.com>
2024-11-20 18:54:48 +0300

    Personel

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Personel
+    {
+        [Key]
+        public int PersonelId { get; set; }
+        public string PersonelAd { get; set; }
+        public string PersonelSoyad { get; set; }
+        public string PersonelGorsel { get; set; }
+    }
+}
