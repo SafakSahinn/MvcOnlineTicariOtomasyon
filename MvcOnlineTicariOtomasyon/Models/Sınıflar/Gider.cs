f46b09b2c68a7865671e8c654888b2a50ea720ae
safak <safaksahin022@gmail.com>
2024-11-21 21:03:49 +0300

    aciklama kısıtlar

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Gider.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Gider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,11 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
     {
         [Key]
         public int GiderId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
         public string Aciklama { get; set; }
+
         public DateTime Tarih { get; set; }
         public decimal Tutar { get; set; }
     }
