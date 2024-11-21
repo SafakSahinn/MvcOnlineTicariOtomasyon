0b16809486c0ab421bcc35f1e958ed8543971243
safak <safaksahin022@gmail.com>
2024-11-21 21:06:11 +0300

    personel kısıtlar

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,8 +11,17 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
     {
         [Key]
         public int PersonelId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string PersonelAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string PersonelSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(250)]
         public string PersonelGorsel { get; set; }
 
         //ilişkiler
