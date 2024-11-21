db04ff68881263d4830bc93861126518251de159
safak <safaksahin022@gmail.com>
2024-11-21 21:02:51 +0300

    faturalar kısıtlamalar

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Faturalar.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Faturalar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +9,31 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
 {
     public class Faturalar
     {
+        [Key]
         public int FaturaId { get; set; }
+
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
         public string FarutaSeriNo { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(6)]
         public string FaturaSıraNo { get; set; }
+
         public DateTime Tarih { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(60)]
         public string VergiDairesi { get; set; }
+
         public DateTime Saat { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string TeslimEden { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string TeslimAlan { get; set; }
 
         //ilişkiler
