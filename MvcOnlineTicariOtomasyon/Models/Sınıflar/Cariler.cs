25c3924ceefd5add0d73320b10f1ea15e27284ce
safak <safaksahin022@gmail.com>
2024-11-21 20:46:07 +0300

    cariler kısıtlamalar

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Cariler.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Cariler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,9 +11,21 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
     {
         [Key]
         public int CariId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string CariAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
         public string CariSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(13)]
         public string CariSehir { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
         public string CariMail { get; set; }
 
         //ilişkiler
