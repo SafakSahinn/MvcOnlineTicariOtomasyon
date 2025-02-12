d70fddd626e765c58c121eed1563f648e0f13f60
safak <safaksahin022@gmail.com>
2025-02-12 18:42:48 +0300

    faturalar

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Faturalar.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Faturalar.cs
@@ -26,7 +26,9 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
         [StringLength(60)]
         public string VergiDairesi { get; set; }
 
-        public DateTime Saat { get; set; }
+        [Column(TypeName = "Char")]
+        [StringLength(5)]
+        public string Saat { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
@@ -35,6 +37,8 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string TeslimAlan { get; set; }
+
+        public decimal Toplam { get; set; }
 
         //ilişkiler
         public ICollection<FaturaKalem> FaturaKalems { get; set; }
